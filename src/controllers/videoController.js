3277a7fb4ca3f8c2c85c8559c561b234e7ec0606
greyhawk16 <hitec16@naver.com>
2023-01-18 15:06:53 +0900

    Routers created
    # 4.0 ~ 4.8
    Created routers, basic architectures

---
+++ src/controllers/videoController.js
@@ -0,0 +1,21 @@
+export const trending = (req, res) => {
+    res.send("Home Page Videos");
+};
+export const see = (req, res) => {
+    console.log(req.params);
+    return res.send("Watch");
+};
+export const edit = (req, res) => {
+    console.log(req.params);
+    res.send("Edit");
+};
+export const search = (req, res) => {
+    res.send("search");
+};
+export const upload = (req, res) => {
+    res.send("Upload");
+};
+export const deleteVideo = (req, res) => {
+    console.log(req.params);
+    res.send("Delete Video");
+};
--- src/server.js
+++ src/server.js
@@ -1,23 +1,21 @@
 import express from "express"
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
+
 const app = express();
-
 const logger = morgan("common");
-
-const home = (req, res, next) => {
-    console.log("hello");
-    return res.send("hello?");
-};
-
-const login = (req, res, next) => {
-    return res.send("login");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+
 
 const handleListening = () => console.log(`Server Listening on port http://localhost:${PORT}`);
 app.listen(4000, handleListening);
