68c1631b121b831f842359c7ed8587198f984ecf
greyhawk16 <hitec16@naver.com>
2023-01-27 10:41:16 +0900

    MongoDB, Mongoose initialized
    #6.5 ~ #6.14
    
    Configured MongoDB, Mongoose for use.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,49 +1,43 @@
-let videos = [
-    {
-        title: "First Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id:1,
-    },
-    {
-        title: "Second Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id:2,
-    },
-    {
-        title: "Third Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id:3,
-    },
-];
+import Video from "../models/Video";
 
 
-export const trending = (req, res) => {
+// Video.find({}, (error, videos) => {
+//     console.log("Search Finished");
+// });
+
+export const home = async (req, res) => {
+    const videos = await Video.find({});
     return res.render("home", { pageTitle: "Home", videos });
 };
+  
+
 export const watch = (req, res) => {
     const { id } = req.params;
     const video = videos[id - 1];
-    return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+    return res.render("watch", { pageTitle: `Watching`});
 };
-export const edit = (req, res) => {
-    res.render("edit");
+
+
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    return res.render("edit", { pageTitle: `Editing`});
 };
-export const search = (req, res) => {
-    res.send("search");
+
+
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    res.redirect(`/videos/${id}`);
 };
-export const upload = (req, res) => {
-    res.send("Upload");
+
+
+export const getUpload = (req, res) => {
+    return res.render("upload", { pageTitle: "upload Video" });
 };
-export const deleteVideo = (req, res) => {
-    console.log(req.params);
-    res.send("Delete Video");
-};
+
+
+export const postUpload = (req, res) => {
+    //add a video to video array
+    const { title } = req.body;
+    return res.redirect("/");
+}
--- src/server.js
+++ src/server.js
@@ -4,18 +4,16 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
+
 const app = express();
 const logger = morgan("common");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + '/src/views');
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-
-
-const handleListening = () => console.log(`Server Listening on port http://localhost:${PORT}`);
-app.listen(4000, handleListening);
+export default app;
