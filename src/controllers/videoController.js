3760886ff4f0ca5ae1285110acb37937576d63d6
greyhawk16 <hitec16@naver.com>
2023-01-31 15:13:56 +0900

    Video Edit and Delete
    #6.20 ~ #6.25
    Enabled videos to be edited(title, description, hashtags) and deleted

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -14,19 +14,35 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
+    if (!video) {
+        return res.render("404", { pageTitle: "Video Not Found" });
+    }
     return res.render("watch", { pageTitle: video.title, video });
 };
 
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const { id } = req.params;
-    return res.render("edit", { pageTitle: `Editing`});
+    const video = await Video.findById(id);
+    if (!video) {
+        return res.render("404", { pageTitle: "Video Not Found" });
+    }
+    return res.render("edit", { pageTitle: `Edit: ${video.title}`, video});
 };
 
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
+    const { title, description, hashtags } = req.body;
+    const video = await Video.exists({ _id: id });
+    if (!video) {
+        return res.render("404", { pageTitle: "Video Not Found" });
+    }
+    await Video.findByIdAndUpdate(id, {
+        title,
+        description,
+        hashtags: Video.formatHashtags(hashtags),
+    });
     res.redirect(`/videos/${id}`);
 };
 
@@ -43,8 +59,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title,
             description,
-            // createdAt: Date.now(),
-            hashtags: hashtags.split(",").map(word => `#${word}`),
+            hashtags: Video.formatHashtags(hashtags),
         });
         return res.redirect("/");
     } catch (error) {
@@ -53,4 +68,11 @@ export const postUpload = async (req, res) => {
             errorMessage: error._message,
         });
     }
+};
+
+
+export const deleteVideo = async (req, res) => {
+    const { id } = req.params;
+    await Video.findByIdAndDelete(id);
+    return res.redirect("/");
 };
