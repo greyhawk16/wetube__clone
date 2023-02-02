b81869eab85f987c9ddbcbc82d99c2b2d096a2fb
greyhawk16 <hitec16@naver.com>
2023-02-02 13:36:05 +0900

    User model created
    #7.1
    Created User model, join form

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,14 @@
-export const join = (req, res) => {
-    res.send("Join");
+export const getJoin = (req, res) => {
+    res.render("join", { pagetitle: "Join" });
 };
+
+
+export const postJoin = (req, res) => {
+    console.log(req.body);
+    res.end();
+};
+
+
 export const edit = (req, res) => {
     res.send("Edit User");
 };
