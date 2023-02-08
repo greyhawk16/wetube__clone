9127af6caf7c94f2e2b5e31d29c7a4517236b0b7
greyhawk16 <hitec16@naver.com>
2023-02-08 11:00:54 +0900

    Logged in screen configured
    # 7.10
    Main screen changes if user logs in

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 
 const app = express();
@@ -21,18 +22,8 @@ app.use(session({
     })
 );
 
-app.use((req, res, next) => {
-    req.sessionStore.all((error, sessions) => {
-        console.log(sessions);
-        next();
-    })
-});
 
-app.get("/add-one", (req, res, next) => {
-    req.session.potato += 1;
-    return res.send(`${req.session.id}\n${req.session.potato}`);
-})
-
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
