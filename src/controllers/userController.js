a16f12de8ed3c4d306277e89785c998b0008a9ae
greyhawk16 <hitec16@naver.com>
2023-02-09 13:05:26 +0900

    Log in with Github
    #7.16 ~ #7.19
    github login configuring

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import User from "../models/User";
+import fetch from "node-fetch";
 
 
 export const getJoin = (req, res) => {
@@ -71,6 +72,54 @@ export const postLogin = async (req, res) => {
     req.session.user = user;
     return res.redirect("/");
 };
+
+
+export const startGithubLogin = (req, res) => {
+    const baseUrl = "https://github.com/login/oauth/authorize";
+    const config = {
+        client_id: process.env.GH_CLIENT,
+        allow_signup: true,
+        scope: "read:user user:email",
+    };
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+};
+
+
+export const finishGithubLogin = async (req, res) => {
+    const baseUrl = `https://github.com/login/oauth/access_token`;
+    const config = {
+        client_id: process.env.GH_CLIENT,
+        client_secret: process.env.GH_SECRET,
+        code: req.query.code,
+    };
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    const tokenRequest = await (
+        await fetch(finalUrl, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+            },
+        })
+    ).json();
+    if ("access_token" in tokenRequest) {
+        //access API
+        const { access_token } = tokenRequest;
+        const userRequest = await(await fetch("https://api.github.com/user", {
+            headers: {
+                    Authorization: `token ${access_token}`,
+
+                },
+            })
+        ).json();
+        console.log(userRequest);
+    } else {
+        return res.redirect("/login");
+    }
+};
+
 
 export const edit = (req, res) => {
     res.send("Edit User");
