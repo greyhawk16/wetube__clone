780aac6f75eebbe33a1ded03153433ccbbf21eba
greyhawk16 <hitec16@naver.com>
2023-02-15 12:56:53 +0900

    Video's owner pt.1
    # 8.11
    Linked video to uploader

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 import Video from "../models/Video";
+import User from "../models/User";
 
 
 // Video.find({}, (error, videos) => {
@@ -14,10 +15,11 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
+    const owner = await User.findById(video.owner);
     if (!video) {
         return res.render("404", { pageTitle: "Video Not Found" });
     }
-    return res.render("watch", { pageTitle: video.title, video });
+    return res.render("watch", { pageTitle: video.title, video, owner, });
 };
 
 
@@ -54,6 +56,7 @@ export const getUpload = (req, res) => {
 
 export const postUpload = async (req, res) => {
     //add a video to video array
+    const { user: { _id }, } = req.session;
     const { path: fileUrl } = req.file;
     const { title, description, hashtags } = req.body;
     try {
@@ -61,6 +64,7 @@ export const postUpload = async (req, res) => {
             title,
             description,
             fileUrl,
+            owner: _id,
             hashtags: Video.formatHashtags(hashtags),
         });
         return res.redirect("/");
