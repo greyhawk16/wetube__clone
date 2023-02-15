1fd97e87b72573df390e982ff9a5be801ec85191
greyhawk16 <hitec16@naver.com>
2023-02-15 10:31:45 +0900

    Video upload
    # 8.9
    Users can upload video files and play it.

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -9,7 +9,11 @@ import {
     getChangePassword,
     postChangePassword,
 } from "../controllers/userController";
-import { protectorMiddleware, publicOnlyMiddleware, uploadFiles } from "../middlewares";
+import {
+    protectorMiddleware,
+    publicOnlyMiddleware,
+    avatarUpload,
+} from "../middlewares";
 
 const userRouter = express.Router();
 
@@ -19,7 +23,7 @@ userRouter
     .route("/edit")
     .all(protectorMiddleware)
     .get(getEdit)
-    .post(uploadFiles.single("avatar"), postEdit);
+    .post(avatarUpload.single("avatar"), postEdit);
 
 userRouter
     .route("/change-password")
