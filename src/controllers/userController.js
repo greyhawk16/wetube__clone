c052cabdce56fc70cfdb29971bd253688659585f
greyhawk16 <hitec16@naver.com>
2023-02-15 13:35:17 +0900

    video owner pt2
    #8.12
    Other user's profile displays videos upload by him/her.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import User from "../models/User";
+import Video from "../models/Video";
 import fetch from "node-fetch";
 
 
@@ -274,9 +275,11 @@ export const see = async (req, res) => {
             pageTitle: "User Not Found",
         });
     }
-
+    const videos = await Video.find({ owner: user._id });
+    // console.log(videos);
     return res.render("users/profile", {
         pageTitle: `${user.name}의 Profile`,
-        user
+        user,
+        videos
     });
 };
