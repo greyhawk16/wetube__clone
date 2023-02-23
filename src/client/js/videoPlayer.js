277a5e4a39bd7112cddd1b7304bdfd69633d6352
greyhawk16 <hitec16@naver.com>
2023-02-23 15:13:37 +0900

    Video playbar
    # 11.6
    Video's Timeline

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,9 +4,13 @@ const muteBtn = document.getElementById("mute");
 const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+
 
 let volumeValue = 0.5
 video.volume = volumeValue;
+
+
 
 const handlePlayClick = (e) => {
     if (video.paused) {
@@ -30,6 +34,8 @@ const handleMute = (e) => {
     volumeRange.value = video.muted ? 0 : volumeValue;
 };
     
+
+
 const handleInputVolume = (event) => {
     const {
         target: { value },
@@ -45,6 +51,8 @@ const handleInputVolume = (event) => {
     video.volume = value;
 };
     
+
+
 const handleChangeVolume = (event) => {
     const {
         target: { value },
@@ -54,16 +62,25 @@ const handleChangeVolume = (event) => {
     }
 };
 
-const formatTime = (sec) => new Date(sec * 1000).toISOString().substring(11, 19);
 
+
+const formatTime = (sec) => new Date(sec * 1000).toISOString().substring(11, 19);
 
 const handleLoadedMetadata = () => {
     totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
 };
 
 const handleTimeUpdate = () => {
     currentTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
 };
+
+
+const handleTimelineChange = (event) => {
+    const { target: { value } } = event;
+    video.currentTime = value;
+}
 
 
 playBtn.addEventListener("click", handlePlayClick);
@@ -72,3 +89,4 @@ volumeRange.addEventListener("input", handleInputVolume);
 volumeRange.addEventListener("change", handleChangeVolume);
 video.addEventListener("loadedmetadata", handleLoadedMetadata)
 video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimelineChange);
