43ce542ba70d9bb2ed4c3fe1d8536573ef54f3cf
greyhawk16 <hitec16@naver.com>
2023-02-27 08:05:52 +0900

    Video Controls pt1
    # 11.8
    Show and hide video control depending on mouse's location

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -7,8 +7,10 @@ const totalTime = document.getElementById("totalTime");
 const timeline = document.getElementById("timeline");
 const fullScreenBtn = document.getElementById("fullScreen");
 const videoContainer = document.getElementById("videoContainer");
+const VideoControls = document.getElementById("VideoControls");
 
 
+let controlsTimeout = null;
 let volumeValue = 0.5
 video.volume = volumeValue;
 
@@ -97,6 +99,22 @@ const handleFullScreen = () => {
 };
 
 
+const handleMouseMove = () => {
+    if (controlsTimeout) {
+        clearTimeout(controlsTimeout);
+        controlsTimeout = null;
+    }
+    VideoControls.classList.add("showing");
+};
+const handleMouseLeave = () => {
+    controlsTimeout = setTimeout(() => {
+        VideoControls.classList.remove("showing");
+    }, 3000);
+
+
+};
+
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleInputVolume);
@@ -105,3 +123,5 @@ video.addEventListener("loadedmetadata", handleLoadedMetadata)
 video.addEventListener("timeupdate", handleTimeUpdate);
 timeline.addEventListener("input", handleTimelineChange);
 fullScreenBtn.addEventListener("click", handleFullScreen);
+video.addEventListener("mousemove", handleMouseMove);
+video.addEventListener("mouseleave", handleMouseLeave);
