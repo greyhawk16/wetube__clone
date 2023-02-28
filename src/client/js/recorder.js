9540cc0ebe69527f49176de431831fbeb14f518f
greyhawk16 <hitec16@naver.com>
2023-02-28 10:56:07 +0900

    Video recording pt.1
    # 13.2
    User can start, stop recording

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,15 +1,44 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
+
+let stream;
+
+const handleStop = () => {
+    startBtn.innerText = "Start Recording";
+    startBtn.removeEventListener("click", handleStop);
+    startBtn.addEventListener("click", handleStart);
+};
 
 
-const handleStart = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia(
+const handleStart = () => {
+    startBtn.innerText = "Stop Recording";
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+
+    const recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (e) => console.log(e);
+    console.log(recorder);
+    recorder.start();
+    console.log(recorder);
+    setTimeout(() => {
+        recorder.stop();
+    }, 10000);
+};
+
+
+const init = async () => {
+    stream = await navigator.mediaDevices.getUserMedia(
         {
-            audio: true,
-            video: true,
+            audio: false,
+            video: {
+                width: 200,
+                height: 100,
+            },
         });
-    
-    console.log(stream);
-}
+    video.srcObject = stream;
+    video.play();
+};
 
 
+init();
 startBtn.addEventListener("click", handleStart);
