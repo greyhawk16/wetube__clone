3f6afbbd4309d2d4cdae0f58965864200e53b920
greyhawk16 <hitec16@naver.com>
2023-03-03 11:04:04 +0900

    Comment models, comment section
    # 16.0 ~ # 16.2
    
    Added Comment models to MongoDB, and created comment section

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
+
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -9,16 +10,19 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String },
     location: String,
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
     videos: [
         { type: mongoose.Schema.Types.ObjectId, ref: "Video" },
     ],
 });
+
 
 userSchema.pre('save', async function () {
     if (this.isModified("password")) {
         this.password = await bcrypt.hash(this.password, 5);
     }
 });
+
 
 const User = mongoose.model('User', userSchema);
 export default User;
--- src/models/Video.js
+++ src/models/Video.js
@@ -5,19 +5,22 @@ const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 80 },
   fileUrl: { type: String, required: true, },
   thumbUrl: { type: String, required: true, },
-  description: {type: String, required: true, trim: true, minLength: 5},
+  description: { type: String, required: true, trim: true, minLength: 5 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
   meta: {
-    views: {type: Number, default: 0, required:true},
-    rating: {type: Number, default: 0, required:true},
+    views: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
   },
+  comments: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: "Comment" }],
   owner: {type:mongoose.Schema.Types.ObjectId, required:true, ref:"User"},
 });
 
+
 videoSchema.static("changePathFormula", (urlPath) => {
   return urlPath.replace(/\\/g, "/");
-  });
+});
+  
 videoSchema.static('formatHashtags', function (hashtags) {
   return hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`))
 });
