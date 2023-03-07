536e3a1ef235c27113141198612ee6d976c520a8
greyhawk16 <hitec16@naver.com>
2023-03-07 09:25:37 +0900

    Real Time Comments
    # 16.7
    Can add comments in real time

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -3,6 +3,24 @@ const form = document.getElementById("commentForm");
 
 
 
+const addComment = (text) => {
+    const videoComments = document.querySelector(".video__comments ul");
+    const newComment = document.createElement("li");
+    newComment.className = "video__comment";
+
+    const icon = document.createElement("i");
+    icon.className = "fas fa-comment";
+
+    const span = document.createElement("span");
+    span.innerText = ` ${text}`;
+        
+    newComment.appendChild(icon);
+    newComment.appendChild(span);
+    videoComments.prepend(newComment);
+};
+
+
+
 const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -14,7 +32,7 @@ const handleSubmit = async (event) => {
         return;
     }
     
-    await fetch(`/api/videos/${videoId}/comment`, {
+    const {status} = await fetch(`/api/videos/${videoId}/comment`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -22,6 +40,9 @@ const handleSubmit = async (event) => {
         body: JSON.stringify({text}),
     });
     textarea.value = "";
+    if (status === 201) {
+        addComment(text);
+    }
 };
 
 
