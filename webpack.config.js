501435175786932946e96ad25aa4d79a8db4bb8c
greyhawk16 <hitec16@naver.com>
2023-03-12 08:53:15 +0900

    Building Frontend
    # 17.3
    Builds frontend that works on any browser environment.

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,6 @@ module.exports = {
         recorder: BASE_JS + "recorder.js",
         commentSection: BASE_JS + "commentSection.js",
     },
-    watch: true,
-    mode: 'development',
     plugins: [new MiniCssExtractPlugin({
         filename:"css/styles.css",
     })],
